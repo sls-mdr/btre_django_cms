88af48a037ba7aee746bfd845227081f0af1441a
Silas Mederer <mederersilas@gmail.com>
2023-11-09 17:32:22 +0100

    feat: add media directory and link to project

--- btre/settings.py
+++ btre/settings.py
@@ -122,10 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "static"
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "btre/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media settings
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"

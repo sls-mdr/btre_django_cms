bbae8e5012bcb9fc0eea9a71eecae434d2246916
Silas Mederer <mederersilas@gmail.com>
2023-11-13 20:07:12 +0100

    feat(listings): adds pagination and humanize numbers and dates

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import Listing
 
 
 def index(request):
-    return render(request, "listings/listings.html")
+    """View for the home page"""
+    listings = Listing.objects.order_by("-list_date").filter(is_published=True)
+    paginator = Paginator(listings, 6)
+    page = request.GET.get("page")
+    page_listings = paginator.get_page(page)
+    context = {"listings": page_listings}
+    return render(request, "listings/listings.html", context)
 
 
-def listing(request):
+def listing(request, listing_id: int):
+    """View for the listing page"""
     return render(request, "listings/listing.html")
 
 
 def search(request):
+    """View for the search page"""
     return render(request, "listings/search.html")

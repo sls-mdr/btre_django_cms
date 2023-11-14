df541eb0fdbac4c5e5ebbfca200ac7e25b6f21a5
Silas Mederer <mederersilas@gmail.com>
2023-11-14 13:55:02 +0100

    feat(search): adds search functionality

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 from .models import Listing
+from .choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
@@ -22,4 +24,43 @@ def listing(request, listing_id: int):
 
 def search(request):
     """View for the search page"""
-    return render(request, "listings/search.html")
+    queryset_list = Listing.objects.order_by("-list_date")
+
+    # Keyword search
+    if "keywords" in request.GET:
+        keywords = request.GET["keywords"]
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City search
+    if "city" in request.GET:
+        city = request.GET["city"]
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # State search
+    if "state" in request.GET:
+        state = request.GET["state"]
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    # Bedroom search
+    if "bedrooms" in request.GET:
+        bedrooms = request.GET["bedrooms"]
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__gte=bedrooms)
+
+    # Price search
+    if "price" in request.GET:
+        price = request.GET["price"]
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        "state_choices": state_choices,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+        "listings": queryset_list,
+    }
+
+    return render(request, "listings/search.html", context)

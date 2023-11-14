c94391a0df3c795b9faeaa2443993eba7113e264
Silas Mederer <mederersilas@gmail.com>
2023-11-14 13:08:20 +0100

    refactor(pages): templates search params

--- pages/views.py
+++ pages/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
     """View for the home page."""
     listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
-    context = {"listings": listings}
+    context = {
+        "listings": listings,
+        "state_choices": state_choices,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+    }
     return render(request, "pages/index.html", context)
 
 

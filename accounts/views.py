0ceb7fd75fe4e86b27d5ceef1a70662f4d46c332
Silas Mederer <mederersilas@gmail.com>
2023-11-15 21:56:02 +0100

    feat(accounts): user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,63 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
-def register(request):
-    """A view that displays the register page"""
+def register(request: object) -> object:
+    """
+    View for user registration.
+
+    Args:
+        request: HTTP request object.
+
+    Returns:
+        HttpResponse: Rendered HTML page or redirection.
+
+    """
     if request.method == "POST":
-        messages.error(request, "Test Error Message")
-        return redirect("register")
+        # Get form values from the POST request
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+
+        # Check if passwords match
+        if not password == password2:
+            messages.error(request, "Passwords do not match")
+            return redirect("register")
+
+        # Check if the username is already taken
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username is already taken")
+            return redirect("register")
+
+        # Check if the email is already taken
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email is already taken")
+            return redirect("register")
+
+        # Create a new user
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        # Login user after creation and redirect to the home page
+        # auth.login(request, user)
+        # messages.success(request, "You are now logged in")
+        # return redirect("index")
+
+        # Display a success message and redirect to login page
+        user.save()
+        messages.success(request, "You are now registered and can log.")
+        return redirect("login")
     else:
+        # Render the registration form if the request method is not POST
         return render(request, "accounts/register.html")
 
 

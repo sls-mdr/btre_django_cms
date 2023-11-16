fa74a1f32084a866b53e16e3edcd4712a0a18cf5
Silas Mederer <mederersilas@gmail.com>
2023-11-16 13:11:02 +0100

    feat(contacts): adds messaging and tests for multi contacts

--- contacts/models.py
+++ contacts/models.py
@@ -7,9 +7,10 @@ class Contact(models.Model):
     listing_id = models.IntegerField()
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100, blank=True)
     message = models.TextField(blank=True)
-    contact_date = models.DateTimeField(default=timezone.now)
+    contact_date = models.DateTimeField(default=timezone.now, blank=True)
+    realtor_email = models.CharField(max_length=100)
     # Inquieries can be send by not registered users
     user_id = models.IntegerField(blank=True)
 
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
 
 
-def contact(request):
-    return
+from django.shortcuts import redirect
+from django.contrib import messages
+from .models import Contact
+
+
+def contact(request: object) -> object:
+    """
+    View for handling contact form submissions.
+
+    Args:
+        request: HTTP request object.
+
+    Returns:
+        HttpResponse: Redirection to the listing page.
+
+    """
+    if request.method == "POST":
+        # Extract form data from the POST request
+        listing_id = request.POST["listing_id"]
+        listing = request.POST["listing"]
+        name = request.POST["name"]
+        email = request.POST["email"]
+        phone = request.POST["phone"]
+        message = request.POST["message"]
+        user_id = request.POST["user_id"]
+        realtor_email = request.POST["realtor_email"]
+
+        # Check if the user is authenticated
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            # Check if the user has already made an inquiry for this listing
+            has_contacted = Contact.objects.filter(
+                listing_id=listing_id, user_id=user_id
+            ).exists()
+            if has_contacted:
+                messages.error(
+                    request, "You have already made an inquiry for this listing."
+                )
+                return redirect("/listings/" + listing_id)
+
+        # Create a new Contact instance and save it to the database
+        contact = Contact(
+            listing=listing,
+            listing_id=listing_id,
+            name=name,
+            email=email,
+            phone=phone,
+            message=message,
+            realtor_email=realtor_email,
+            user_id=user_id,
+        )
+
+        contact.save()
+        messages.success(
+            request, "Your request has been submitted. A realtor will get back to you."
+        )
+
+        return redirect("/listings/" + listing_id)
+    else:
+        # If the request method is not POST, do nothing
+        pass

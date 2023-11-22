4b9cbeb07208a2672c70466cfff98915a3273d29
Silas Mederer <mederersilas@gmail.com>
2023-11-22 13:26:34 +0100

    feat(accounts): adds dashborad functionality

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
+from contacts.models import Contact
+
 
 def register(request: object) -> object:
     """
@@ -105,4 +107,8 @@ def logout(request):
 
 def dashboard(request):
     """A view that displays the dashboard page"""
-    return render(request, "accounts/dashboard.html")
+    user_contacts = Contact.objects.order_by("-contact_date").filter(
+        user_id=request.user.id
+    )
+    context = {"contacts": user_contacts}
+    return render(request, "accounts/dashboard.html", context)
